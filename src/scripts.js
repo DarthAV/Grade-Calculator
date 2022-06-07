f6b2a83b3dad9774c2e05c55469804991b116048
Ansh Verma <ansh.verma22@outlook.com>
2022-06-07 13:06:17 -0700

    code up calculations

--- src/scripts.js
+++ src/scripts.js
@@ -32,17 +32,47 @@ function calculate() {
 
 }
 
+
+// take input from html form
+function getInput() {
+    currentGrade = document.getElementById("currentGrade").value;
+    possiblePointsAssignment = document.getElementById("possiblePointsAssignment").value;
+    weight = document.getElementById("weight").value;
+    possiblePointsCategory = document.getElementById("possiblePointsCategory").value;
+    currentPointsCategory = document.getElementById("currentPointsCategory").value;
+}
+
+// calculate other categories grade
+function caclulateOther() {
+    if (weight != 100) {
+        otherGrades = currentGrade - (currentPointsCategory / possiblePointsCategory) * weight;
+    }
+}
+
 // calculate percent in the class based on points on the assignment
 function calculatePercent(points) {
+    return ((points + currentPointsCat) / (possiblePointsCat + possiblePoints) * weight) + otherGrades;
 }
 
 // round to n decimal places
 function round(value, decimals) {
+    return Number(Math.round(value + 'e' + decimals) + 'e-' + decimals);
 }
 
-// calculate other grades
-function caclulateOther() {
-    if (weight != 100) {
-}
-}
+//minimize possible score you can get
+function minimizePossibleScore(possiblePoints) {
+    var currentLowestPoints = possiblePoints;
+    var percentWithLowScore = 100.0;
 
+    for (var i = possiblePoints; i >= 0; i -= 0.1) {
+
+        var newGrade = calculatePercent(i);
+        if (newGrade < 90) {
+            break;
+        }
+        percentWithLowScore = calculatePercent(i);
+        currentLowestPoints = i;
+
+    }
+    return lowestPoints;
+}
